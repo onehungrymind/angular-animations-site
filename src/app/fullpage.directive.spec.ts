e77eb18cfd18b63cb5d17895ec6bed24e3267961
Lukas Ruebbelke <simpul@gmail.com>
2017-04-16 21:05:20 -0700

    fixing spec

--- src/app/fullpage.directive.spec.ts
+++ src/app/fullpage.directive.spec.ts
@@ -1,8 +1,12 @@
 import { FullpageDirective } from './fullpage.directive';
 
+const elementRefMock = {
+  nativeElement: {}
+}
+
 describe('FullpageDirective', () => {
   it('should create an instance', () => {
-    const directive = new FullpageDirective();
+    const directive = new FullpageDirective(elementRefMock);
     expect(directive).toBeTruthy();
   });
 });
